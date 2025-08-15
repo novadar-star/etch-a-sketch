c9a7587a7af85d7a67818cd2d5cc0c8d85c2fb41
Darla Nova <jolynekujoismywife06@gmail.com>
2025-08-16 02:46:31 +0800

    created columns and rows via deisgning it using flexbox especially using flex:1

--- js.js
+++ js.js
@@ -1,24 +1,20 @@
 
 function createDiv(size){
     const outerscreen = document.querySelector(".container");
-
     for(let i=0; i<size; i++){
         const col = document.createElement("div");
         col.classList.add("col");
             for(let j=0; j<size;j++){
                 const row = document.createElement('div');
                 row.classList.add("row");
-               // row.style.width="5px";
-               // row.style.height="5px";
-               // row.style.background = "pink";
+            
                 row.style.border = "1px solid blue";
-                row.innerText = (i * size) + j;
-                row.classList.add("row");
+                row.innerText = (i * size) + j; //trackig
                 col.appendChild(row);
             }
                 col.style.background = "pink";
                 col.style.border = "1px solid black";
-            outerscreen.appendChild(col);
+                outerscreen.appendChild(col);
         }
     
     
