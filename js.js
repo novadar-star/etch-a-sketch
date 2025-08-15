50b8dac11881653e8ed98b5d9bce9ce793a79fbb
Darla Nova <jolynekujoismywife06@gmail.com>
2025-08-16 02:33:07 +0800

    created box with rows

--- js.js
+++ js.js
@@ -1,20 +1,27 @@
-const container = document.querySelector(".container");
 
-function createDiv(row){
-    for(let i=0; i<row; i++){
-    
-        const box = document.createElement('div');
-        box.style.width = "10px";
-        box.style.height = "10px";
-        box.style.background = "pink";
-        box.style.border = "1px solid #AA336A";
-        container.classList.add("box");
-        container.appendChild(box);
+function createDiv(size){
+    const outerscreen = document.querySelector(".container");
+
+    for(let i=0; i<size; i++){
+        const col = document.createElement("div");
+        col.classList.add("col");
+            for(let j=0; j<size;j++){
+                const row = document.createElement('div');
+                row.classList.add("row");
+               // row.style.width="5px";
+               // row.style.height="5px";
+               // row.style.background = "pink";
+                row.style.border = "1px solid blue";
+                row.innerText = (i * size) + j;
+                row.classList.add("row");
+                col.appendChild(row);
+            }
+                col.style.background = "pink";
+                col.style.border = "1px solid black";
+            outerscreen.appendChild(col);
         }
     
     
 }
 
-
-
-createDiv(16)
+createDiv(5)
