5b076fdc8b5f3be54eb7f593785b7d492c84c105
Darla Nova <jolynekujoismywife06@gmail.com>
2025-08-17 00:05:37 +0800

    Relocate buttons color into the side panel

--- js.js
+++ js.js
@@ -1,23 +1,27 @@
-createDiv(15)
+createDiv(10)
 function createDiv(size){
     const outerscreen = document.querySelector(".container");
+    const sketchpad = document.querySelector(".sketchpad");
     for(let i=0; i<size; i++){
         const col = document.createElement("div");
         col.classList.add("col");
             for(let j=0; j<size;j++){
                 const row = document.createElement('div');
                 row.classList.add("row");
-                row.style.border = "1px solid white";
+                row.style.border = "1px solid white"; //add to button size
+
                 col.appendChild(row);
             }
                 col.style.background = "pink";
-                col.style.border= "1px solid white";
+//                col.style.border= "1px solid white";
                 col.style.borderLeft= "none";
                 col.style.borderRight= "none";
-                col.style.borderTop= "none";
+                
+
                 col.style.borderBottom= "none";
                 outerscreen.appendChild(col);
         }
+        sketchpad.style.borderTop = "1px solid black";
     
     
 }
